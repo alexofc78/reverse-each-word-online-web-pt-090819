dc6cb22d0d7aa772f8752cf2b34242d9f7910495
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-21 17:55:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 #end
 
 #def reverse_each_word(array)
-  array = "Hello there, and how are you?".split
-  new_array = array.split
-  new_array.reverse
-end
+#  array = "Hello there, and how are you?".split
+#  new_array = array.split
+#  new_array.reverse
+#end
